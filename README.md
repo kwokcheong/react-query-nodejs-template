16292f891888c5568defbf0d1359a986d92807d4
Kwok Cheong <wongkwokcheong@hotmail.com>
2024-02-02 23:32:23 +0800

    added components navbar as example

---
+++ README.md
@@ -0,0 +1,23 @@
+# reactjs-nodejs-sequelize_template
+
+This is a template fullstack web app using reactJS nodeJS express and Sequelize with mySQL
+
+Technologies:
+
+1. Built with npx create-react-app, not vite sorry D':
+2. Using tailwind css and flowbite
+3. using sequelize
+4. Refer to flowbite for all the components: https://flowbite.com/docs/components/
+
+# Using TailwindCss and Flowbite
+
+In client, run these command:
+
+- `npx tailwindcss -i ./src/styles/input.css -o ./src/styles/output.css --watch`
+- `npm start`
+
+In server, run this command:
+
+- `nodemon start`
+
+# Using Sequelize and MySql:
---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import "./styles/input.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useGetRootData } from "./hooks/api/useGetRootData";
+import { Card } from "./components/Card";
+import { NavBar } from "./components/NavBar";
+
+const queryClient = new QueryClient();
+
+// function Example() {
+//   const { isPending, error, data, isFetching } = useGetRootData();
+
+//   if (isPending) return "Loading...";
+
+//   if (error) return "An error has occurred: " + error.message;
+
+//   return (
+//     <div>
+//       <pre>{JSON.stringify(data)}</pre>
+//       <div>{isFetching ? "Updating..." : ""}</div>
+//     </div>
+//   );
+// }
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NavBar />
+      <div className="flex flex-col items-center justify-center">
+        <div>
+          <Card />
+        </div>
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
