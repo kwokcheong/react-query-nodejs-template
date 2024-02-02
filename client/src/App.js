42fcd494dc7a4c2822f30fe57f9dc24c187a539a
Kwok Cheong <wongkwokcheong@hotmail.com>
2024-02-02 23:37:39 +0800

    added jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useGetRootData } from "./hooks/api/useGetRootData";
 import { Card } from "./components/Card";
 import { NavBar } from "./components/NavBar";
+import { Jumbotron } from "./components/Jumbotron";
 
 const queryClient = new QueryClient();
 
@@ -27,7 +28,7 @@ function App() {
       <NavBar />
       <div className="flex flex-col items-center justify-center">
         <div>
-          <Card />
+          <Jumbotron />
         </div>
       </div>
     </QueryClientProvider>
